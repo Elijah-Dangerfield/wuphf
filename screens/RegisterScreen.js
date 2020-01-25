aa1dddbe98c22f6445313f4cbc1b8fde0e906900
Elijah Dangerfield <elijahdangerfield111@gmail.com>
2020-01-25 10:35:15 -0600

    adds register base

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,19 +1,81 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import styled from "styled-components";
+import SmallFormEntry from "../components/SmallFormEntry";
+import AppButton from "../components/AppButton";
+import { ScrollView } from "react-native";
 
-export default function RegisterScreen() {
+const RegisterScreen = props => {
+  const [info, setInfo] = useState({
+    email: "",
+    phone: "",
+    password: "",
+    confirmPassword: ""
+  });
   return (
-    <View style={styles.container}>
-      <Text>Register</Text>
-    </View>
+    <ScrollView>
+      <Container>
+        <Title>Register</Title>
+        <SmallFormEntry
+          hint="Enter your email"
+          title="Email"
+          password={false}
+          onChangeText={text => {
+            setInfo({ ...info, email: text });
+          }}
+        />
+        <SmallFormEntry
+          hint="Enter your phone number"
+          title="Phone Number"
+          password={false}
+          onChangeText={text => {
+            setInfo({ ...info, phone: text });
+          }}
+        />
+        <SmallFormEntry
+          hint="Enter a password"
+          title="Password"
+          password={true}
+          onChangeText={text => {
+            setInfo({ ...info, password: text });
+          }}
+        />
+        <SmallFormEntry
+          hint="re-enter password"
+          title="Confirm Password"
+          password={true}
+          onChangeText={text => {
+            setInfo({ ...info, confirmPassword: text });
+          }}
+        />
+        <ButtonWrapper>
+          <AppButton
+            title="Register"
+            textColor="white"
+            backgroundColor="#FC6C00"
+            onPress={() => {
+              console.log(info);
+              // props.navigation.push("Message");
+            }}
+          />
+        </ButtonWrapper>
+      </Container>
+    </ScrollView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+export default RegisterScreen;
+const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+const Title = styled.Text`
+  font-size: 42px;
+  font-weight: 600;
+  margin: 20px 0 20px 20px;
+  color: #584798;
+`;
+
+const ButtonWrapper = styled.View`
+  height: 150px;
+  margin-top: 50px;
+`;
