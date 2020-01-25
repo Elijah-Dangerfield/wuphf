3fdf21e3ad76045dc7dc5588b60d3e95aad5242b
Elijah Dangerfield <elijahdangerfield111@gmail.com>
2020-01-25 17:23:27 -0600

    adds scroll view to login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,71 +2,75 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import SmallFormEntry from "../components/SmallFormEntry";
 import AppButton from "../components/AppButton";
-import * as firebase from "firebase"
-import {ScrollView, Alert} from "react-native";
+import * as firebase from "firebase";
+import { ScrollView, Alert } from "react-native";
 
-export function build_alert(title, msg){
+export function build_alert(title, msg) {
   Alert.alert(
     title,
     msg,
     [
       {
-        text: 'Cancel',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel',
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel"
       },
-      {text: 'OK', onPress: () => console.log('OK Pressed')},
+      { text: "OK", onPress: () => console.log("OK Pressed") }
     ],
-    {cancelable: false},
+    { cancelable: false }
   );
 }
 
 const LoginScreen = props => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  var loginFail = false
+  var loginFail = false;
   return (
-    <Container>
-      <Title>Log In</Title>
-      <SmallFormEntry
-        hint="Enter username"
-        title="Username"
-        password={false}
-        onChangeText={text => {
-          setUsername(text);
-        }}
-      />
-      <SmallFormEntry
-        onChangeText={text => {
-          setPassword(text);
-        }}
-        password={true}
-        hint="Enter password"
-        title="Password"
-      />
-      <ButtonWrapper>
-        <AppButton
-          title="Log In"
-          textColor="white"
-          backgroundColor="#FC6C00"
-          
-          onPress={() => {
-            console.log(username);
-            console.log(password);
-            firebase.auth().signInWithEmailAndPassword(username, password)
-              .catch(err => {
-                console.log(err)
-                build_alert("Login Error", "Error with login credentials!")
-                loginFail = true
-              })
-              .finally(() => {
-                if(!loginFail){props.navigation.push("Message")}
-              })
-            
+    <ScrollView>
+      <Container>
+        <Title>Log In</Title>
+        <SmallFormEntry
+          hint="Enter username"
+          title="Username"
+          password={false}
+          onChangeText={text => {
+            setUsername(text);
           }}
         />
-      </ButtonWrapper>
-    </Container>
+        <SmallFormEntry
+          onChangeText={text => {
+            setPassword(text);
+          }}
+          password={true}
+          hint="Enter password"
+          title="Password"
+        />
+        <ButtonWrapper>
+          <AppButton
+            title="Log In"
+            textColor="white"
+            backgroundColor="#FC6C00"
+            onPress={() => {
+              console.log(username);
+              console.log(password);
+              firebase
+                .auth()
+                .signInWithEmailAndPassword(username, password)
+                .catch(err => {
+                  console.log(err);
+                  build_alert("Login Error", "Error with login credentials!");
+                  loginFail = true;
+                })
+                .finally(() => {
+                  if (!loginFail) {
+                    props.navigation.push("Message");
+                  }
+                });
+            }}
+          />
+        </ButtonWrapper>
+      </Container>
+    </ScrollView>
   );
 };
 
